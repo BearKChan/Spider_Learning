8dbd38440a84365298e6e3c016e489f63ca86e8f
bearkchan <bearkchan@gmail.com>
2018-04-28 10:11:28 +0800

    init commit

---
+++ seleniums.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+#
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+#
+#
+# driver = webdriver.PhantomJS(executable_path="D:\Coding\Python\localenv\phantomjs-2.1.1-windows\\bin\phantomjs.exe")
+# driver.get("http://www.baidu.com")
+#
+# driver.save_screenshot("baidu.png")
+
+import pytesseract
+
+help(pytesseract)
